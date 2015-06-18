c296a90b0799ad603ceb942c06b6a8b3ec0f6b30
Maik-C <maik.couwenberg@live.nl>
2015-06-18 23:37:03 +0200

    test_runs
    fixed some grievous errors
    works with some test values in it
    next commit should have nrg_in and nrg_out defined

--- MatLab Code/simulation.m
+++ MatLab Code/simulation.m
@@ -2,14 +2,16 @@
 
 %constants
 total_var = 5; %5 because mood has been pre-set
-
+base_weight = 70; %starting weight of all agents
+base_height = 1.75; %starting height of all agents
+test_val = 3096; %test value for nrg in and enegery out
 
 %input handling
 
 total_year = input('how long should the simulation last?');
 total_agents = input('how many agents are simulated?');
 
-display = fprintf('The simulation will run: %d Agents and take: %d Years', total_agents, total_year);
+display = fprintf('The simulation will run: %d Agents and take: %d Years\n', total_agents, total_year);
 
 %matrix data structure for agent simulation
 %constants for: mood nrg_in nrg_out BMI and weight
@@ -38,13 +40,28 @@ while (year_count < total_year)
     %based on current variables:
     
     temp = zeros(total_var, total_agents); %creates empty 6 by n agents matrix ready to be filled
-    for i = 1:total_agents 
-        %2set nrg_in for agent i
-        %3set nrg_out for agent i
-        %4calc nrg_netto for agent i (energy expenditure)
-        %5calc weight
-        temp(5, i) = 78;
-        %6calc BMI               
+    for i = 1:total_agents
+        if(day_count == 1)
+            temp(4,i) = base_weight;
+            temp(5,i) = (temp(4, i)/(base_height)^2);
+        else
+            if(day_count == 2)
+                old_weight = agent_days(6);
+            else
+                old_weight = agent_days(6+total_var);
+            end
+            %2set nrg_in for agent i
+            nrg_in = test_val;
+            %3set nrg_out for agent i
+            nrg_out = test_val;
+            %4calc nrg_netto for agent i (energy expenditure)
+            nrg_netto = nrg_in - nrg_out;
+            temp(3,i) = nrg_netto;
+            %5calc weight
+            temp(4,i) = old_weight + (base_height * 0.6)+(nrg_netto/7716);
+            %6calc BMI
+            temp(5,i) = (temp(4,i)/(base_height)^2);
+        end
     end
     %append the now filled temp matrix to agent_days
     agent_days = [agent_days;temp]; 
