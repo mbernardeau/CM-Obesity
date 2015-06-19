bd0d55737dcdab43c40c335292a6b3efe46f4644
GabrielleRM <gabriellermp@hotmail.com>
2015-06-19 14:03:24 +0200

    Update set_expen.m

--- MatLab Code/set_expen.m
+++ MatLab Code/set_expen.m
@@ -4,23 +4,24 @@ function [expen] = set_expen(old_weight, mood, intake)
 
     
     lower = 1;
-    upper = 3;
     if(mood == 2)
         upper = 6;
+    elseif(mood == 3)
+        upper = 3;
     else
         upper = 9;
     end
     
     int_activity = round((lower + (upper-lower*rand(1))));
-    if(int_activity < 3) %hi intensity
-        enrg_out = 3 * old_weight;
+    if(int_activity < 3) %high intensity
+        nrg_out = 3 * old_weight;
     elseif(int_activity < 6 && int_activity > 2) %med intensity
-        enrg_out = 2 * old_weight;
+        nrg_out = 2 * old_weight;
     else %low intensity
-        enrg_out = 1 *old_weight;
+        nrg_out = 1 *old_weight;
     end
     
-    expen = ((879+11.6+enrg_out)*old_weight)+(0.1*intake);
+    expen = 854+((10.15+nrg_out)*old_weight)+(0.1*intake);
     
     
 end
