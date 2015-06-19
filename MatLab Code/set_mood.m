f64647f66d33a04c02e3884bc09333492462bac3
GabrielleRM <gabriellermp@hotmail.com>
2015-06-19 14:01:18 +0200

    Update set_mood.m

--- MatLab Code/set_mood.m
+++ MatLab Code/set_mood.m
@@ -1,7 +1,8 @@
 function [state] = set_mood()
     
-    upper = 3;
-    mood = round(rand(1)*upper);
+    lower = 1;
+    upper = 2;
+    mood = lower+round(rand(1)*upper);
     %mood 1 = 'happy' | mood 2 = 'neutral' | mood 3 = 'depressed'
     state = mood;
 end
